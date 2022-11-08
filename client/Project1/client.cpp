2086017b0fd3d77478eda1f381b426e039d057af
ZJPZJY <2530064451@qq.com>
2022-11-08 19:19:58 +0800

    pro3

---
+++ client/Project1/client.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include"frame.h"
+#include<winsock.h>
+#pragma comment(lib, "WS2_32")
+int main() {
+	//windowsÆô¶¯socket·þÎñ
+	WSADATA wsaData;
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		printf("WSAStartup() error!\n");
+
+	SOCKET s = socket(AF_INET, SOCK_STREAM, 0);
+	SOCKET new_s;
+	struct sockaddr_in saddr, new_addr;
+
+
+	memset(&saddr, 0, sizeof(saddr));
+	saddr.sin_family = AF_INET;
+	saddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	saddr.sin_port = htons(1234);
+
+	if (connect(s, (SOCKADDR*)&saddr, sizeof(saddr)) == -1) {
+		printf("connect feiled\n");
+		return 0;
+	}
+	char* message = (char*)malloc(sizeof(char) * 20);
+	recv(s, message, 20, 0);
+	fram* fra = (fram*)message;
+	printf("%d", fra->MESS_LEN);
+	return 0;
+}
