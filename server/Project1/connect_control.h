5a2fb4bf48294bd0d07cf304e8278585461e6f61
ZJPZJY <2530064451@qq.com>
2022-11-08 19:18:39 +0800

    pro3

--- server/Project1/connect_control.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-enum connect_status {
-	unconnect,
-	connected,
-	clos,
-};
-enum connect_status status = unconnect;
--- server/Project1/file_opt.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#define FILE_MAX_LEN 1473
-struct file_opt
-{
-	char type;
-	char filename[20];
-	char mess[FILE_MAX_LEN];
-};
